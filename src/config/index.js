0bf638302224d863b9fcd26751372a3b688e7550
mardefronteira <mmv341@nyu.edu>
2022-03-09 15:58:18 -0300

    atualização db 03/2022

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const DB_URL = window.location.hostname.includes('192.168.100.174')
-? 'http://192.168.100.174:8080'
+const DB_URL = window.location.hostname.includes('localhost')
+? 'http://localhost:8080'
 : 'https://paranaflix.herokuapp.com';
 
 export default {
